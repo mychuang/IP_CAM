f51b228edc37567b11d10cf73ac56398fef223e9
Miller.Huang <Miller.Huang@lumens.com.tw>
2021-07-27 16:48:04 +0800

    the used lib backup

---
+++ x64_backup/modifyPermission/uac.bat
@@ -0,0 +1,5 @@
+@echo off
+mt.exe -manifest uac.manifest -outputresource:devSearch.exe";#1
+echo 操作完成！
+echo.
+pause
---
+++ x64_backup/modifyPermission/uac__.bat
@@ -0,0 +1,5 @@
+@echo off
+mt.exe -manifest uac.manifest -outputresource:Ladibug.msi";#1
+echo 操作完成！
+echo.
+pause
